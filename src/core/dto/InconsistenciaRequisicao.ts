2424d6410190bbef51b90cf4e89772bd9ef18e3c
warley.xavier <warley.xavier@eagletecnologia.com.br>
2021-02-21 19:05:52 -0300

    Correção em formatação do código

--- src/core/dto/InconsistenciaRequisicao.ts
+++ src/core/dto/InconsistenciaRequisicao.ts
@@ -2,8 +2,8 @@ export default class InconsistenciaRequisicao {
   public constructor(propriedade: string, inconsistencias: any) {
     this.propriedade = propriedade;
     this.messages = Object.keys(inconsistencias).map(propriedade => inconsistencias[propriedade]);
-  };
+  }
 
   propriedade: string;
   messages: string[];
-};
+}
