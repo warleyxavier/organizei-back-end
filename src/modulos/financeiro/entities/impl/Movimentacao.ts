a0f0e9223b797378d3e3a90f4bcb9b1bdc41c4bf
warley.xavier <warley.xavier@eagletecnologia.com.br>
2021-03-01 20:38:07 -0300

    feat:Implementado mapeamento de movimentacoes financeiras

---
+++ src/modulos/financeiro/entities/impl/Movimentacao.ts
@@ -0,0 +1,36 @@
+import { Service } from "typedi";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+import ICategoria from "../ICategoria";
+import Categoria from "./Categoria";
+import IConta from "../IConta";
+import Conta from "./Conta";
+import IMovimentacao from "../IMovimentacao";
+
+@Service({id: "movimentacao", transient: true})
+@Entity("movimentacoes")
+export default class Movimentacao implements IMovimentacao {
+
+  @PrimaryGeneratedColumn({name: "id"})
+  Codigo: number;
+
+  @Column({name: "descricao"})
+  Descricao: string;
+
+  @Column({name: "valor"})
+  Valor: number;
+
+  @Column({name: "data"})
+  Data: Date;
+
+  @Column({name: "ordem"})
+  Ordem: number;
+  
+  @JoinColumn({name: "conta_id", referencedColumnName: "Codigo"})
+  @ManyToOne<Conta>(() => Conta)
+  Conta: IConta;
+  
+  @JoinColumn({name: "categoria_id", referencedColumnName: "Codigo"})
+  @ManyToOne<Categoria>(() => Categoria)
+  Categoria: ICategoria;
+}
