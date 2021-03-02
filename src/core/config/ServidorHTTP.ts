56489467ce1b40395b3c7f98d61e78a39826f906
warley.xavier <warley.xavier@eagletecnologia.com.br>
2021-03-01 21:51:05 -0300

    teste 1 cors

--- src/core/config/ServidorHTTP.ts
+++ src/core/config/ServidorHTTP.ts
@@ -4,6 +4,7 @@ import { json } from "body-parser";
 import { TratamentoExcecoesMiddleware } from "../../middlewares/TratamentosExcecoesMiddleware";
 
 const app = createExpressServer({
+	cors: false,
 	defaultErrorHandler: false,
 	controllers: [__dirname + "/../../modulos/**/controllers/*.{ts,js}"],
 	middlewares: [TratamentoExcecoesMiddleware],
