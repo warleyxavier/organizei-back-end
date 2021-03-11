5cbad3e5803225f6d2190807131af0b678fa1392
warley.xavier <warley.xavier@eagletecnologia.com.br>
2021-03-10 21:51:28 -0300

    feat: Implementada consulta de objetivos

--- src/modulos/financeiro/controllers/ObjetivoFinanceiroController.ts
+++ src/modulos/financeiro/controllers/ObjetivoFinanceiroController.ts
@@ -1,4 +1,4 @@
-import { Body, JsonController, Post, Req, UseBefore } from "routing-controllers";
+import { Body, Get, JsonController, Post, Req, UseBefore } from "routing-controllers";
 import Container from "typedi";
 
 import AutenticacaoMiddleware from "../../../middlewares/AutenticacaoMiddleware";
@@ -7,6 +7,7 @@ import ObjetivoParaInsercaoDto from "../dto/ObjetivoFinanceiroParaInsercaoDto";
 import ObjetivoFinanceiroParaConsultaDto from "../dto/ObjetivoFinanceiroParaConsultaDto";
 import MapeadorDeObjetivoFinanceiro from "../mapeadores/MapeadorDeObjetivoFinanceiro";
 import IGerenciadorObjetivoFinanceiro from "../service/IGerenciadorObjetivoFinanceiro";
+import { runInThisContext } from "vm";
 
 @JsonController("/objetivos")
 @UseBefore(AutenticacaoMiddleware)
@@ -26,6 +27,13 @@ export default class ObjetivoFinanceiroController {
     const objetivo = this.mapeador.paraEntidade(dto);
     const objetivoInserido = await this.gerenciadorObjetivo.criar(objetivo, codigoUsuario);
     return this.mapeador.paraDto(objetivoInserido);
+  }
+
+  @Get("/")
+  public async pesquisar(@Req() request: any): Promise<ObjetivoFinanceiroParaConsultaDto[]> {
+    const { codigoUsuario } = request;
+    const objetivos = await this.gerenciadorObjetivo.pesquisar(codigoUsuario);
+    return this.mapeador.paraListaDto(objetivos);    
   }
 
 }
